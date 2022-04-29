57e53bb758a211cd4fb6bf14c506ca54956fcfbb
violog <51th.apprent1ce.f0rce@gmail.com>
2022-04-29 13:16:21 +0300

    Synthetic base method (not working)
    The problem is not considering restriction ALL(Xi>=0). For some LPP from
    conspectus that works fine, but this doesn't work for LPP of my variant:
    negative values mustn't be in result.

--- simplex.go
+++ simplex.go
@@ -4,23 +4,27 @@ import (
 	"fmt"
 	"log"
 	"math"
+	"os"
 )
 
 type simplex struct {
 	// Описание условия задачи
-	targetF      []float64   // коэффициенты при переменных целевой функции (ЦФ)
-	targetFValue float64     // её значение (свободный член)
+	targetF []float64 // коэффициенты при переменных целевой функции (ЦФ)
+	//targetFTerm float64     // свободный член ЦФ
+	// if everything works out, I'll leave targetFValue
+	targetFValue float64     // текущее значение ЦФ
 	argsMatrix   [][]float64 // коэффициенты при аргументах системы ограничений (СО)
 	baseValues   []float64   // значения базисных переменных (свободных членов СО)
 	min          bool        // экстремум ЦФ: true - min, false - max
 	// Параметры, получаемые в ходе расчётов
 	baseNumbers []int     // номера текущих базисных переменных
-	theta       []float64 // временный параметр для выбора разрешающей строки
-	lastRow     []float64 // оценки влияния переменных на ЦФ
+	theta       []float64 // временный параметр для выбора направляющей строки
+	delta       []float64 // оценки влияния свободных переменных на ЦФ
+	deltaM      []float64 // оценки влияния коэф. при М на ЦФ
 }
 
-// getLPPFromArgs Получить ЗЛП из набора соответствующих аргументов
-func getLPPFromArgs(targetF []float64, targetFValue float64, argsMatrix [][]float64, baseValues []float64, min bool) *simplex {
+// GetLPPFromArgs Получить ЗЛП из набора соответствующих аргументов
+func GetLPPFromArgs(targetF []float64, targetFValue float64, argsMatrix [][]float64, baseValues []float64, min bool) *simplex {
 	return &simplex{
 		targetF:      targetF,
 		targetFValue: targetFValue,
@@ -40,49 +44,37 @@ func (s *simplex) convertToPreferredView() {
 			s.baseValues[i] = -v
 		}
 	}
-}
-
-// setBase Находит потенциальные столбцы для ед. матрицы и превращает её ненулевые элементы в единицы, если требуется;
-// устанавливает базис из переменных, соответствующих этим столбцам
-func (s *simplex) setBase() {
-	// т.к. задача в каноническом виде, ищем столбцы по признаку "все нули, кроме одного"
-	for j := 0; j < len(s.argsMatrix[0]); j++ {
-		var (
-			chosenCol    bool
-			chosenVal    float64
-			chosenValRow int
-			foundNonZero bool
-		)
-		// проверяем каждый элемент столбца
-		for i := 0; i < len(s.argsMatrix); i++ {
-			if s.argsMatrix[i][j] != 0 {
-				// если больше одного ненулевого элемента, пропустить столбец
-				if foundNonZero {
-					chosenCol = false
-					break
-				}
-				foundNonZero, chosenCol = true, true
-				chosenVal = s.argsMatrix[i][j]
-				chosenValRow = i
-			}
-		}
-		if !foundNonZero {
-			log.Fatalln("Найден столбец с нулями, что делать?")
-		}
-		if chosenCol {
-			// если столбец с нулями, но ненулевой элемент != 1, разделить строку на него
-			// WARN это может быть неверным!
-			if chosenVal != 1 {
-				myMap(s.argsMatrix[chosenValRow], func(n float64) float64 { return n / chosenVal })
-				// не забыть разделить и базисные переменные
-				s.baseValues[chosenValRow] /= chosenVal
-			}
-			if len(s.baseNumbers) == 0 {
-				s.baseNumbers = make([]int, len(s.argsMatrix))
-			}
-			s.baseNumbers[chosenValRow] = j + 1
-		}
+	//for j:=0;j<len(s.argsMatrix);j++{}
+	//var added uint
+	//getAppendingArray := func() []float64 {
+	//	arr := make([]float64, len(s.argsMatrix))
+	//	// Единица на месте соответствующей искусственной переменной
+	//	arr[added] = 1
+	//	return arr
+	//}
+	// Добавляем искусственный базис
+	m := len(s.argsMatrix)
+	// Получаем список значений для вставки в строку-ограничение (вынесено из цикла для оптимизации)
+	arr := make([]float64, m)
+	s.baseNumbers = make([]int, m)
+	for added, row := range s.argsMatrix {
+		// Единица на месте соответствующей искусственной переменной
+		arr[added] = 1
+		s.argsMatrix[added] = append(row, arr...)
+		// Обнулить для последующей итерации
+		arr[added] = 0
+		//added++
+		// Делаем переменную базисной
+		s.baseNumbers[added] = len(row) + added + 1
+		//if added < m-1 {
+		// Нельзя получить константную длину, поэтому приходится брать длину след. строки
+		//s.baseNumbers[added] = len(s.argsMatrix[m-1]) + added + 1
+		//continue
+		//}
+		// В последнем ограничении номер баз. перем. равен текущей длине
+		//s.baseNumbers[added] = len(row)
 	}
+	// todo what to do with s.targetF? Should I add synthetic vars with 0?
 }
 
 func myMap[T any](arr []T, f func(T) T) {
@@ -102,47 +94,72 @@ func (s *simplex) printTable() {
 			fmt.Printf("%-7v", 0)
 		} else {
 			fmt.Printf("%-6.2g ", n)
-			// note: see usage of %g/%G when ugly out is produced
 		}
+	}
+	printM := func() { // sync with printFloat
+		if s.min {
+			fmt.Printf("%-6s ", "M")
+		} else {
+			fmt.Printf("%-6s ", "-M")
+		}
+	}
+	printX := func(n int) {
+		fmt.Printf("X%-5d ", n) // sync with printFloat
 	}
 	printRow := func(row []float64) {
 		for _, v := range row {
 			printFloat(v)
 		}
 	}
-	printSpaces := func(spaces int) {
-		for spaces > 0 {
+	printSpaces := func(count int) {
+		for count > 0 {
 			fmt.Print(" ")
-			spaces--
+			count--
 		}
 	}
+	m := len(s.argsMatrix)
 	// 1. Выводим заголовок таблицы
 	fmt.Println()
 	// пробелы для коэф. при ЦФ сверху
 	printSpaces(len(partHeader))
 	// сами коэф. при ЦФ
 	printRow(s.targetF)
+	// коэф. при искусственных переменных
+	//if len(s.baseNumbers) > 0 {
+	for i := 0; i < len(s.baseNumbers); i++ {
+		printM()
+	}
+	//}
 	fmt.Printf("\n%s", partHeader)
 	// переменные ЦФ X(i)
-	for i := 0; i < len(s.targetF); i++ {
-		fmt.Printf("X%-5d ", i+1) // sync with printFloat
+	for i := 1; i <= len(s.targetF); i++ {
+		printX(i)
+	}
+	// искусств. пер. X(i)
+	for i := len(s.targetF) + 1; i <= len(s.argsMatrix[0]); i++ {
+		printX(i)
 	}
 	fmt.Println("theta")
-	// 2. Строки таблицы
-	for i := 0; i < len(s.argsMatrix); i++ {
+	// 2. Основные строки таблицы
+	for i := 0; i < m; i++ {
 		fmt.Printf("%-2d ", i+1)
 		// если базис не ещё установлен, заполнить пробелами, иначе вывести его
-		if len(s.baseNumbers) < len(s.argsMatrix) {
+		if len(s.baseNumbers) < m {
 			printSpaces(len(partHeader) - 3 - 7)
 		} else {
 			bnum := s.baseNumbers[i]
 			fmt.Printf("X%-2d  ", bnum)
-			printFloat(s.targetF[bnum-1])
+			// коэф. при иск. пер. вывести отдельно
+			if bnum > len(s.targetF) {
+				printM()
+			} else {
+				printFloat(s.targetF[bnum-1])
+			}
 		}
 		printFloat(s.baseValues[i])
 		printRow(s.argsMatrix[i])
 		if i < len(s.theta) {
-			if s.theta[i] < 0 {
+			if s.theta[i] <= 0 {
 				fmt.Printf("%-6s", "-")
 			} else {
 				printFloat(s.theta[i])
@@ -150,29 +167,37 @@ func (s *simplex) printTable() {
 		}
 		fmt.Println()
 	}
-	// 3. Последняя строка
+	// 3. Вектор оценок для свободных переменных
 	// необходимые пробелы до столбца Х
-	fmt.Printf("%d  ", len(s.argsMatrix)+1)
-	printSpaces(len(partHeader) - 3 - 7)
-	// значение и инвертированные коэф. ЦФ
-	printFloat(s.targetFValue)
-	//printRow(s.targetF, true)
-	printRow(s.lastRow)
+	if len(s.delta) > 0 {
+		fmt.Printf("%d  ", m+1)
+		printSpaces(len(partHeader) - 3 - 7)
+		// значение и инвертированные коэф. ЦФ
+		printFloat(s.targetFValue)
+		printRow(s.delta)
+	}
+	fmt.Println()
+	// 4. Вектор оценок для коэф. при М
+	if len(s.deltaM) > 0 {
+		fmt.Printf("%d  ", m+2)
+		printSpaces(len(partHeader) - 3 - 7)
+		printRow(s.deltaM)
+	}
 	fmt.Println()
 }
 
 // isOptimal Проверяет план на оптимальность (SRP важнее производительности в данный момент)
 func (s *simplex) isOptimal() bool {
-	// при поиске минимума ЦФ план оптимален, если все значения <=0
 	if s.min {
-		for _, v := range s.lastRow {
+		// при поиске минимума ЦФ план оптимален, если все значения <=0
+		for _, v := range s.delta {
 			if v > 0 {
 				return false
 			}
 		}
-		// при поиске максимума - все >=0
 	} else {
-		for _, v := range s.lastRow {
+		// при поиске максимума - все >=0
+		for _, v := range s.delta {
 			if v < 0 {
 				return false
 			}
@@ -181,67 +206,152 @@ func (s *simplex) isOptimal() bool {
 	return true
 }
 
-// getResolvingColumnNumber - возвращает номер разрешающего столбца, начиная с 0
-// optimization: if returns 0 as min or max, plan is optimal
-func (s *simplex) getResolvingColumnNumber() (n int) {
+// getDirectiveColumnNumber - возвращает номер направляющего, начиная с 0
+func (s *simplex) getDirectiveColumnNumber() (number int) {
+	// к-во неискусственных переменных
+	n := len(s.argsMatrix[0]) - len(s.argsMatrix)
+	var syntheticVarsInBase bool
+	// Проверяем наличие иск. пер. в базисе
+	for _, v := range s.baseNumbers {
+		if v > len(s.targetF) {
+			syntheticVarsInBase = true
+			break
+		}
+	}
 	if s.min {
+		// Ищем макс. положительное значение среди оценок
 		max := -math.MaxFloat64
-		for i, v := range s.lastRow {
+		// В обоих векторах оценок не учитываем значения при искусственных переменных,
+		// т. к. они не должны возвращаться в базис
+		for i, v := range s.delta[:n] {
 			if v > max {
 				max = v
-				n = i
+				number = i
+				// Другой вариант алгоритма состоит в возвращении первого попавшегося значения
+				// Здесь ищем максимум среди всех
 			}
+		}
+		// Если есть иск. пер., проверяем последнюю строку, иначе игнорируем
+		if syntheticVarsInBase {
+			// Сдвигаем массив вправо на 1 из-за свободного члена
+			for i, v := range s.deltaM[1 : n+1] {
+				if v > max {
+					max = v
+					number = i
+				}
+			}
+		}
+		// Если макс. неположителен, план оптимален
+		if max <= 0 {
+			return -1
 		}
 	} else {
 		min := math.MaxFloat64
-		for i, v := range s.lastRow {
+		// Ищем мин. по тем же принципам
+		for i, v := range s.delta[:n] {
 			if v < min {
 				min = v
-				n = i
+				number = i
 			}
+		}
+		if syntheticVarsInBase {
+			for i, v := range s.deltaM[1 : n+1] {
+				if v < min {
+					min = v
+					number = i
+				}
+			}
+		}
+		// Если мин. неотрицателен, план оптимален
+		if min >= 0 {
+			return -1
 		}
 	}
 	return
 }
 
-// setLastRow Переписать коэф. из ЦФ в последнюю строку, инвертировав их
-func (s *simplex) setLastRow() {
-	s.lastRow = make([]float64, len(s.targetF))
+// setDeltas Установить векторы оценок для свободных членов и коэф. при М
+func (s *simplex) setDeltas() {
+	// К-во ограничений и неискусственных переменных
+	m := len(s.argsMatrix)
+	n := len(s.argsMatrix[0]) - m
+	// Для свободных членов просто инвертируем коэф. при ЦФ
+	s.delta = make([]float64, len(s.targetF)+m)
 	for i, v := range s.targetF {
-		s.lastRow[i] = -v
+		s.delta[i] = -v
 	}
+	// Установим оценки для коэф. при М
+	s.deltaM = make([]float64, len(s.targetF)+m+1)
+	// Если выразить остальные переменные через искусственные, то коэф.
+	// при каждой из первых будет равен инвертированной сумме коэф.
+	// во всех ограничениях, а свободный член будет инвертирован
+	//coefs := make([]float64, n+1)
+	// Устанавливаем свободный член
+	for _, v := range s.baseValues {
+		s.deltaM[0] -= v
+	}
+	// Инвертируем сумму значений для соответствующих переменных
+	for j := 1; j <= n; j++ {
+		for i := 0; i < m; i++ {
+			s.deltaM[j] -= s.argsMatrix[i][j-1]
+		}
+	}
+	// Искусственные переменные остаются нулевыми
 }
 
-// getResolvingRowNumber Номер разрешающей строки, начиная с 0
-func (s *simplex) getResolvingRowNumber(resColNumber int) (n int) {
+// getDirectiveRowNumber Номер направляющей строки, начиная с 0
+func (s *simplex) getDirectiveRowNumber(resColNumber int) (number int) {
 	s.theta = make([]float64, len(s.argsMatrix))
-	// Найдём theta делением свободных членов на разрешающий столбец
+	// В первую очередь выведем искусственные переменные
+	for i, v := range s.baseNumbers {
+		// Их номера больше к-ва аргументов в ЦФ (мы не добавляли иск. пер. в неё)
+		if v > len(s.targetF) {
+			return i
+		}
+	}
+	// Найдём theta делением свободных членов на напр. столбец
 	for i, row := range s.argsMatrix {
+		// Защита от деления на 0 (отрицательные значения не отображаются)
+		if row[resColNumber] == 0 {
+			s.theta[i] = -1
+			fmt.Printf("Внимание: попытка деления на 0 при получении theta, столбец X%v\n", resColNumber+1)
+			continue
+		}
 		s.theta[i] = s.baseValues[i] / row[resColNumber]
 	}
 	min := math.MaxFloat64
-	// Находим неотрицательный минимум - это будет номером разр. строки
+	minFound := false
+	// Находим положительный минимум - это будет номером напр. строки
 	for i, v := range s.theta {
-		if v >= 0 && v < min {
+		if v > 0 && v <= min {
+			minFound = true
 			min = v
-			n = i
+			number = i
 		}
+	}
+	if !minFound {
+		dir := "сверху"
+		if s.min {
+			dir = "снизу"
+		}
+		fmt.Println("Задача не имеет решений: функция не ограничена", dir)
+		os.Exit(0)
 	}
 	return
 }
 
-// setZerosInResolvingColumn Путём матричных преобразований получить нули в разрешающем столбце
-func (s *simplex) setZerosInResolvingColumn(col, row int) {
+// setZerosInColumn Путём матричных преобразований получить нули в направляющем столбце
+func (s *simplex) setZerosInColumn(col, row int) {
 	var multiplier float64
 	// получить нули в матрице аргументов
 	for i, r := range s.argsMatrix {
-		// пропускаем разрешающую строку
+		// пропускаем напр. строку
 		if i == row {
 			continue
 		}
 		// проверяем, чтобы не складывать строку с нулями
 		if r[col] != 0 {
-			// на какое значение умножить разреш. строку, чтобы прибавить её к текущей строке и получить 0 в разреш. столбце?
+			// на какое значение умножить напр. строку, чтобы прибавить её к текущей строке и получить 0 в напр. столбце?
 			multiplier = -r[col] / s.argsMatrix[row][col]
 			sumRows(s.argsMatrix[i], s.argsMatrix[row], multiplier)
 			// сложить соответствующие строкам значения базисных переменных
@@ -249,11 +359,19 @@ func (s *simplex) setZerosInResolvingColumn(col, row int) {
 		}
 	}
 	// получить ноль в оценках - аналогично
-	if v := s.lastRow[col]; v != 0 {
+	if v := s.delta[col]; v != 0 {
 		multiplier = -v / s.argsMatrix[row][col]
-		sumRows(s.lastRow, s.argsMatrix[row], multiplier)
+		sumRows(s.delta, s.argsMatrix[row], multiplier)
 		// учёт значения ЦФ при сложении
 		s.targetFValue += s.baseValues[row] * multiplier
+	}
+	// в оценках коэф. при М аналогично, с оговорками
+	if v := s.deltaM[col+1]; v != 0 {
+		multiplier = -v / s.argsMatrix[row][col]
+		// не учитываем первый элемент (свободный член)
+		sumRows(s.deltaM[1:], s.argsMatrix[row], multiplier)
+		// его складываем отдельно
+		s.deltaM[0] += s.baseValues[row] * multiplier
 	}
 }
 
@@ -268,51 +386,83 @@ func sumRows(forAdding, added []float64, mult float64) {
 	}
 }
 
+// newBase Заменить базисную переменную
+func (s *simplex) newBase(popped, pushed int) {
+	// Делаем базис единичным
+	if v := s.argsMatrix[popped][pushed]; v != 1 {
+		if v == 0 {
+			log.Fatalf("Попытка деления на ноль: popped=%v, pushed=%v\n", popped, pushed)
+		}
+		myMap(s.argsMatrix[popped], func(n float64) float64 { return n / v })
+		// не забываем разделить значения баз. пер.
+		s.baseValues[popped] /= v
+	}
+	s.baseNumbers[popped] = pushed + 1
+	// Ищем выводимую переменную и вводим новую на её место
+	//for i, v := range s.baseNumbers {
+	//	if v == popped+1 {
+	//		s.baseNumbers[i] = pushed + 1
+	//		break
+	//	}
+	//}
+}
+
 // Solve Собственно решение задачи
 func (s *simplex) Solve() {
 	// подготовка к решению
-	s.setLastRow()
-	fmt.Print("Канонический вид:")
+	fmt.Print("Канонический вид (условие задачи):")
 	s.printTable()
 	s.convertToPreferredView()
 	fmt.Print("\nПредпочтительный вид:")
 	s.printTable()
-	s.setBase()
-	fmt.Print("\nУстановлен базис:")
+	s.setDeltas()
+	fmt.Print("\nНайдены оценки:")
 	s.printTable()
-	fmt.Println("\nПлан неоптимален, пытаемся улучшить")
-	for i := 1; !s.isOptimal(); i++ {
-		resCol := s.getResolvingColumnNumber()
-		resRow := s.getResolvingRowNumber(resCol)
-		s.setZerosInResolvingColumn(resCol, resRow)
-		fmt.Printf("\nИтерация %v\nРазрешающий столбец: X%v\nРазрешающая строка: %v", i, resCol+1, resRow+1)
-		s.printTable()
-		s.setBase()
-		fmt.Print("Установлен базис:")
-		s.printTable()
-		// выход из бесконечного цикла (отладка)
-		if i >= 10 {
-			fmt.Println("слишком много итераций")
-			return
+	// Проверим, что план не оптимален
+	if resCol := s.getDirectiveColumnNumber(); resCol != -1 {
+		fmt.Println("\nПлан неоптимален, пытаемся улучшить")
+		for i := 1; resCol != -1; i++ {
+			resRow := s.getDirectiveRowNumber(resCol)
+			s.setZerosInColumn(resCol, resRow)
+			fmt.Printf("\nИтерация %v\nРазрешающий столбец: X%v\nРазрешающая строка: %v", i, resCol+1, resRow+1)
+			s.printTable()
+			s.newBase(resRow, resCol)
+			fmt.Print("Установлен базис:")
+			s.printTable()
+			// Вычисляем след. напр. столбец
+			resCol = s.getDirectiveColumnNumber()
+			// note выход из бесконечного цикла (отладка)
+			if i >= 10 {
+				fmt.Println("слишком много итераций")
+				return
+			}
 		}
+	} else {
+		fmt.Println("\nПлан уже оптимален")
 	}
+	fmt.Println()
 	s.printCurrentAnswer()
+	fmt.Print("\n\n")
 }
 
 // printCurrentAnswer Вывод вектора аргументов и значения ЦФ
 func (s *simplex) printCurrentAnswer() {
-	fmt.Print("Ответ: X* = (")
 	answer := make([]float64, len(s.targetF))
 	// записать в ответ значения базисных переменных, остальные по умолчанию равны 0
 	for i, v := range s.baseNumbers {
+		if v > len(s.targetF) {
+			fmt.Println("Задача не имеет решений: искусственные переменные остались в базисе")
+			os.Exit(0)
+		}
 		answer[v-1] = s.baseValues[i]
 	}
+	fmt.Print("Ответ: X* = (")
 	// вывести вектор
 	for i, v := range answer {
-		fmt.Print(v)
+		fmt.Printf("%.4g", v)
 		if i < len(answer)-1 {
 			fmt.Print(", ")
 		}
 	}
-	fmt.Printf("); Fmin = %v.\n", s.targetFValue)
+	fmt.Printf("); Fmin = %.4g.\n", s.targetFValue)
 }
